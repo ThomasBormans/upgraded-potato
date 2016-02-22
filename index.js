cff2b83f6da946f7f63234102628d566c441e5f4
Thomas Bormans <thomas.bormans@district01.be>
2016-02-22 21:43:23 +0100

    First attempt for payload helper

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict';
 
 // Require all lib's
+var payload = require('./lib/payload');
 var safeJsonParse = require('./lib/safeJsonParse');
 
 // Export each lib
 module.exports = {
+	payload: payload,
 	safeJsonParse: safeJsonParse
 };
